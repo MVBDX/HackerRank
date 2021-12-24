68f35d32f220a3768c1e861cfbe36341a1f36298
MVBDX <faalian.m@gmail.com>
2021-12-24 11:07:39 +0330

    Day 12: Inheritance

--- src/Person.java
+++ src/Person.java
@@ -1,50 +1,73 @@
-import java.util.*;
 
-public class Person {
-    private int age;
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
 
-    public Person(int initialAge) {
-        // Add some more code to run some checks on initialAge
-        // int age;
-        if (initialAge >= 0)
-            age = initialAge;
-        else {
-            age = 0;
-            System.out.println("Age is not valid, setting age to 0.");
-        }
+    // Constructor
+    Person(String firstName, String lastName, int identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
     }
 
-    public void amIOld() {
-        // Write code determining if this person's age is old and print the correct
-        // statement:
-        String toPrint;
-        if (age < 13)
-            toPrint = "You are young.";
-        else if (age >= 13 && age < 18)
-            toPrint = "You are a teenager.";
+    // Print person data
+    public void printPerson() {
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + "\nID: " + idNumber);
+    }
+
+}
+
+class Student extends Person {
+    private int[] testScores;
+
+    /*
+     * Class Constructor
+     * 
+     * @param firstName - A string denoting the Person's first name.
+     * 
+     * @param lastName - A string denoting the Person's last name.
+     * 
+     * @param id - An integer denoting the Person's ID number.
+     * 
+     * @param scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
+
+    Student(String firstName, String lastName, int idNumber, int[] testScores) {
+        super(firstName, lastName, idNumber);
+        this.testScores = testScores;
+    }
+
+    /*
+     * Method Name: calculate
+     * 
+     * @return A character denoting the grade.
+     */
+    // Write your method here
+    char calculate() {
+        int avg;
+        int i, sum = 0;
+        for (i = 0; i < testScores.length; i++) {
+            sum += testScores[i];
+        }
+        avg = sum / i;
+
+        if (avg < 40)
+            return 'T';
+        else if (avg < 55)
+            return 'D';
+        else if (avg < 70)
+            return 'P';
+        else if (avg < 80)
+            return 'A';
+        else if (avg < 90)
+            return 'E';
+        else if (avg <= 100)
+            return 'O';
         else
-            toPrint = "You are old.";
-        System.out.println(toPrint);
-    }
-
-    public void yearPasses() {
-        // Increment this person's age.
-        age += 1;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
-            int age = sc.nextInt();
-            Person p = new Person(age);
-            p.amIOld();
-            for (int j = 0; j < 3; j++) {
-                p.yearPasses();
-            }
-            p.amIOld();
-            System.out.println();
-        }
-        sc.close();
+            return 0;
     }
 }
