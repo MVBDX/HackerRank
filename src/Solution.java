6927ec51731f93493f8ac8b9ba0a0651ca28a27b
MVBDX <faalian.m@gmail.com>
2021-12-24 11:39:17 +0330

    resolve #1

--- src/Solution.java
+++ src/Solution.java
@@ -14,14 +14,34 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String S = bufferedReader.readLine();
+        List<List<Integer>> arr = new ArrayList<>();
 
-        try {
-            System.out.println(Integer.parseInt(S));
-        } catch (Exception e) {
-            System.out.println("Bad String");
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList()));
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int max = -64, temp;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                temp = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) +
+                        arr.get(i + 1).get(j + 1) +
+                        arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+                if (temp > max) {
+                    max = temp;
+                }
+            }
         }
+        System.out.println(max);
 
         bufferedReader.close();
+
     }
 }
