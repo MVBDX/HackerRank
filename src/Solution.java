a988a96521ed300e803e55d83014b71cda923320
MVBDX <faalian.m@gmail.com>
2021-12-24 10:31:06 +0330

    Format Document

--- src/Solution.java
+++ src/Solution.java
@@ -4,15 +4,17 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int consec1=0;
+        int consec1 = 0;
         String strBinary = Integer.toString(n, 2);
-       // above is binary of given in in string
+        // above is binary of given in in string
         String[] strparts = strBinary.split("0");
-        //above we get all strings of ones, below find max from them
-        for(int i=0;i<strparts.length;i++ ){
-            if (consec1 < strparts[i].length()) {consec1 = strparts[i].length();}
+        // above we get all strings of ones, below find max from them
+        for (int i = 0; i < strparts.length; i++) {
+            if (consec1 < strparts[i].length()) {
+                consec1 = strparts[i].length();
+            }
         }
-        System.out.println(consec1);  //result
-        in.close();//sdf
+        System.out.println(consec1); // result
+        in.close();
     }
 }
