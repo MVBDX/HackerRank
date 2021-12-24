ec3736052e74a53b7445a94c0d3d917e68c37bd3
MVBDX <faalian.m@gmail.com>
2021-12-24 11:14:29 +0330

    Day 13: Abstract Classes

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,62 @@
 import java.util.*;
 
-class Solution {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String firstName = scan.next();
-        String lastName = scan.next();
-        int id = scan.nextInt();
-        int numScores = scan.nextInt();
-        int[] testScores = new int[numScores];
-        for (int i = 0; i < numScores; i++) {
-            testScores[i] = scan.nextInt();
-        }
-        scan.close();
+abstract class Book {
+    String title;
+    String author;
 
-        Student s = new Student(firstName, lastName, id, testScores);
-        s.printPerson();
-        System.out.println("Grade: " + s.calculate());
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+
+    // Declare your class here. Do not use the 'public' access modifier.
+    // Declare the price instance variable
+
+    /**
+     * Class Constructor
+     * 
+     * @param title  The book's title.
+     * @param author The book's author.
+     * @param price  The book's price.
+     **/
+    // Write your constructor here
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    /**
+     * Method Name: display
+     * 
+     * Print the title, author, and price in the specified format.
+     **/
+    // Write your method here
+    @Override
+    void display() {
+        // TODO Auto-generated method stub
+        System.out.println("Title: " + this.title);
+        System.out.println("Author: " + this.author);
+        System.out.println("Price: " + this.price);
+    }
+    // End class
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
     }
 }
